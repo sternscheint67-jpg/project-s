a47f1a4d48eede80663a82f397af3e0175fee9f8
sternscheint67-jpg <stern.scheint67@gmail.com>
2026-08-24 14:56:27 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+var CACHE_NAME = 'settsu-regi-cache-v1';
+var CACHE_FILES = ['./', './index.html'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(names.filter(function(name) {
+        return name !== CACHE_NAME;
+      }).map(function(name) {
+        return caches.delete(name);
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      var networkFetch = fetch(event.request).then(function(response) {
+        if (response && response.ok) {
+          var copy = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) { cache.put(event.request, copy); });
+        }
+        return response;
+      }).catch(function() { return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
